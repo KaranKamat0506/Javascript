0661091b567ff9aac6c817b1196f2474403e81a8
unknown <karankamat2000@gmail.com>
2019-01-02 20:34:21 +0530

    Second commit

---
+++ index.js
@@ -0,0 +1,10 @@
+operations();
+function operations(){
+    window.alert("JS file is working");
+    var a=10,b=5;
+    document.write("Addition=" + " " + (a+b));
+    document.write("Subtraction=" + " " + (a-b));
+    document.write("Multiplication=" + " " + (a*b));
+    document.write("Division=" + " " + (a/b));
+}
+
